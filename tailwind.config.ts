84ab6b7bfab17744a7719012d263543de0ca8676
jieun, kim <nueizkk@gmail.com>
2024-09-24 17:27:32 +0900

    Initial commit from Create Next App

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss';
+import tailwindcssForms from '@tailwindcss/forms';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+    },
+  },
+  plugins: [tailwindcssForms],
+};
+export default config;
