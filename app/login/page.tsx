0dbc783616a1414757768a8e7fa864061d96828e
jieun, kim <nueizkk@gmail.com>
2024-09-25 03:12:17 +0900

    route handler(기존의 api route 방식)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,17 @@
+'use client';
+
 import FormInput from '@/components/form-input';
 import FormButton from '@/components/form-btn';
 import SocialLogin from '@/components/social-login';
 
 export default function Login() {
+  const onClick = async () => {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ username: 'abc', password: '123' }),
+    });
+    console.log(await response.json());
+  };
   return (
     <div className='flex flex-col gap-10 py-8 px-6'>
       <div className='flex flex-col gap-2 *:font-medium'>
@@ -17,8 +26,10 @@ export default function Login() {
           required
           errors={[]}
         />
-        <FormButton loading={false} text='Log in' />
       </form>
+      <span onClick={onClick}>
+        <FormButton loading={false} text='Log in' />
+      </span>
       <SocialLogin />
     </div>
   );
