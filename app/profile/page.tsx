497f2deaab1230db1dfd4c506c038ae024faef1d
jieun, kim <nueizkk@gmail.com>
2024-09-27 05:54:48 +0900

    feat: 로그아웃 추가

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,33 @@
-export default function Profile() {
-  return <div>hi</div>;
+import db from '@lib/db';
+import getSession from '@lib/session';
+import { notFound, redirect } from 'next/navigation';
+
+async function getUser() {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({ where: { id: session.id } });
+    if (user) {
+      return user;
+    }
+  }
+  notFound();
+}
+
+export default async function Profile() {
+  const user = await getUser();
+
+  const logout = async () => {
+    'use server';
+    const session = await getSession();
+    session.destroy();
+    redirect('/');
+  };
+  return (
+    <div>
+      <h1>welcome! {user.username}</h1>
+      <form action={logout}>
+        <button>Log out</button>
+      </form>
+    </div>
+  );
 }
