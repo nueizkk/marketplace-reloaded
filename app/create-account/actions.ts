9171e9ae1829b428343879717c3fcc49ad69525b
jieun, kim <nueizkk@gmail.com>
2024-09-27 01:02:08 +0900

    feat: create-account server action 추가

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,8 +2,11 @@
 
 import { type typeToFlattenedError, z } from 'zod';
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX } from '@lib/constants';
+import db from '@lib/db';
+import bcrypt from 'bcrypt';
+import { redirect } from 'next/navigation';
+import getSession from '@lib/session';
 
-// const usernameSchema = z.string().min(2).max(15);
 const checkUsername = (username: string) => !username.startsWith('admin');
 const checkPassword = ({
   password,
@@ -12,6 +15,21 @@ const checkPassword = ({
   password: string;
   confirm_password: string;
 }) => password === confirm_password;
+
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: { username },
+    select: { id: true },
+  });
+  return !Boolean(user);
+};
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: { email },
+    select: { id: true },
+  });
+  return !Boolean(user);
+};
 
 const formSchema = z
   .object({
@@ -22,14 +40,16 @@ const formSchema = z
       })
       .trim()
       .toLowerCase()
-      .refine(checkUsername, '이름은 admin으로 시작할 수 없습니다.'),
+      .refine(checkUsername, '이름은 admin으로 시작할 수 없습니다.')
+      .refine(checkUniqueUsername, '이미 등록된 이름입니다.'),
     email: z
       .string({
         invalid_type_error: '문자로 입력해주세요.',
         required_error: '이메일을 입력해주세요.',
       })
       .email()
-      .toLowerCase(),
+      .toLowerCase()
+      .refine(checkUniqueEmail, '이미 등록된 이메일입니다.'),
     password: z
       .string({
         required_error: '비밀번호를 입력해주세요.',
@@ -74,16 +94,32 @@ export async function createAccount(
     password: formData.get('password'),
     confirm_password: formData.get('confirm_password'),
   };
-  //   usernameSchema.parse(data.username);
-  //   try {
-  //     formSchema.parse(data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  const result = formSchema.safeParse(data);
+
+  const result = await formSchema.safeParseAsync(data);
+
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    // hash password: npm i bcrypt @types/bcrypt
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    console.log(hashedPassword);
+
+    // save the user to db
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: { id: true },
+    });
+
+    // log the user in: npm i iron-session (암호화, 복호화)
+    const session = await getSession();
+    session.id = user.id;
+    await session.save();
+
+    // redirect "/home"
+    redirect('/profile');
   }
 }
---
+++ lib/session.ts
@@ -0,0 +1,13 @@
+import { getIronSession } from 'iron-session';
+import { cookies } from 'next/headers';
+
+interface SessionContent {
+  id?: number;
+}
+
+export default function getSession() {
+  return getIronSession<SessionContent>(cookies(), {
+    cookieName: 'blah',
+    password: process.env.COOKIE_PASSWORD!,
+  });
+}
