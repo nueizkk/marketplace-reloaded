61c2e2a685bb13a0e0c6c7b108b782bcaa6c0128
jieun, kim <nueizkk@gmail.com>
2024-09-27 03:24:53 +0900

    refactor: superRefine -> query 단계적으로 보내도록 수정

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -8,6 +8,7 @@ import { redirect } from 'next/navigation';
 import getSession from '@lib/session';
 
 const checkUsername = (username: string) => !username.startsWith('admin');
+
 const checkPassword = ({
   password,
   confirm_password,
@@ -15,21 +16,6 @@ const checkPassword = ({
   password: string;
   confirm_password: string;
 }) => password === confirm_password;
-
-const checkUniqueUsername = async (username: string) => {
-  const user = await db.user.findUnique({
-    where: { username },
-    select: { id: true },
-  });
-  return !Boolean(user);
-};
-const checkUniqueEmail = async (email: string) => {
-  const user = await db.user.findUnique({
-    where: { email },
-    select: { id: true },
-  });
-  return !Boolean(user);
-};
 
 const formSchema = z
   .object({
@@ -40,16 +26,14 @@ const formSchema = z
       })
       .trim()
       .toLowerCase()
-      .refine(checkUsername, '이름은 admin으로 시작할 수 없습니다.')
-      .refine(checkUniqueUsername, '이미 등록된 이름입니다.'),
+      .refine(checkUsername, '이름은 admin으로 시작할 수 없습니다.'),
     email: z
       .string({
         invalid_type_error: '문자로 입력해주세요.',
         required_error: '이메일을 입력해주세요.',
       })
-      .email()
-      .toLowerCase()
-      .refine(checkUniqueEmail, '이미 등록된 이메일입니다.'),
+      .email('이메일 형식이 올바르지 않습니다.')
+      .toLowerCase(),
     password: z
       .string({
         required_error: '비밀번호를 입력해주세요.',
@@ -74,6 +58,36 @@ const formSchema = z
   .refine(checkPassword, {
     message: '비밀번호가 일치하지 않습니다.',
     path: ['confirm_password'],
+  })
+  .superRefine(async ({ username }, ctx) => {
+    const user = await db.user.findUnique({
+      where: { username },
+      select: { id: true },
+    });
+    if (user) {
+      ctx.addIssue({
+        code: 'custom',
+        message: '이미 등록된 이름입니다.',
+        path: ['username'],
+        fatal: true,
+      });
+      return z.NEVER;
+    }
+  })
+  .superRefine(async ({ email }, ctx) => {
+    const user = await db.user.findUnique({
+      where: { email },
+      select: { id: true },
+    });
+    if (user) {
+      ctx.addIssue({
+        code: 'custom',
+        message: '이미 등록된 이메일입니다.',
+        path: ['email'],
+        fatal: true,
+      });
+      return z.NEVER;
+    }
   });
 
 export async function createAccount(
@@ -119,7 +133,7 @@ export async function createAccount(
     session.id = user.id;
     await session.save();
 
-    // redirect "/home"
+    // redirect "/profile"
     redirect('/profile');
   }
 }
