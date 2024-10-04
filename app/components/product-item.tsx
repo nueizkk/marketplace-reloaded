cea516686ac8fcc3fb4eaf0514ed152859b94d9f
jieun, kim <nueizkk@gmail.com>
2024-10-04 18:05:32 +0900

    fix: navigation 링크 오류 수정

--- app/components/product-item.tsx
+++ app/components/product-item.tsx
@@ -17,7 +17,7 @@ export default function ProductItem({
   id,
 }: ListProductProps) {
   return (
-    <Link href={`products/${id}`} className='flex gap-5'>
+    <Link href={`/products/${id}`} className='flex gap-5'>
       <div className='relative size-28 rounded-md overflow-hidden'>
         <Image fill src={photo} alt={title} className='object-cover' />
       </div>
